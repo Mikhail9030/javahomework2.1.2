5f2a43f82db68af074f1fc3cf9dad191262645ac
Mikhail9030 <gulaevmihail112@gmail.com>
2020-12-28 22:55:24 +0300

    Commit 3

--- MainV3.java
+++ MainV3.java
@@ -1,11 +1,10 @@
 public class MainV3 {
     public static void main(String[] args) {
-        boolean registered = true;
         int balance = 100;
-        int refill = 200;
+        int refill = 1100;
         int bonus = refill / 100;
         int total = balance + refill + bonus;
-        if (balance > 1000) {
+        if (refill > 1000) {
             bonus = refill / 100;
         } else {
             bonus = 0;
