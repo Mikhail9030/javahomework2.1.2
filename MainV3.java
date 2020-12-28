34d80fc6037cc45820d5b0f40383880183eb84e7
Mikhail9030 <gulaevmihail112@gmail.com>
2020-12-28 22:08:12 +0300

    Commit 2

--- MainV3.java
+++ MainV3.java
@@ -1,19 +1,15 @@
 public class MainV3 {
     public static void main(String[] args) {
         boolean registered = true;
-        int percent;
-        if (registered) {
-            percent = 10;
-        } else {
-            percent = 0;
-        }
-        int amount = 1100;
         int balance = 100;
-        int bonus = balance + amount * percent / 100 / 100;
-        if (amount > balance) {
-            balance = bonus;
+        int refill = 200;
+        int bonus = refill / 100;
+        int total = balance + refill + bonus;
+        if (balance > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
         }
-        int total = amount + balance + percent;
         System.out.println(total);
     }
 }
